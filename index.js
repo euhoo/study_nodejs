515a7c8e70cf00142559cf9fb739f6b903044167
Evgeniy Kvach <ypkvach@mts.ru>
2020-12-20 15:50:31 +0300

    adds

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import chalk from "chalk";
 import express from "express";
+import cluster from 'cluster';
+import crypto from 'crypto';
 
-const doWork = (duration) => {
-	 const start = Date.now();
-	 while(Date.now() - start < duration) {}
-}
-
+//ab -c 50 -n 500 localhost:3000/fast
+console.log('Is master cluster: ', cluster.isMaster);
 const app = express();
 
-app.get('/', (req, res) => {
-	console.log(chalk.blue('started'));
-	doWork(5000);
-	console.log(chalk.blue('finished'));
-	res.send('Hi there');
-})
-const port = 3000
-app.listen(port);
-console.log(`Server listening at port:${chalk.green(port)}`);
+if (cluster.isMaster) {
+	cluster.fork()
+} else {
+	app.get('/', (req, res) => {
+		crypto.pbkdf2('a', 'b', 100000, 1024, 'sha512', () => {
+			res.send('Hi there');
+		})
+	})
+	app.get('/fast', (req, res) => {
+		res.send('Hi there');
+	})
+	const port = 3000
+	app.listen(port);
+	console.log(`Server listening at port:${chalk.green(port)}`);
+}
