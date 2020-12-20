974f4ac839d080159c7f9e23a1373e7205f82545
Evgeniy Kvach <ypkvach@mts.ru>
2020-12-20 20:50:35 +0300

    first cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,38 +1,53 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-
+const redis = require('redis');
 const Blog = mongoose.model('Blog');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
-    });
+	app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+		const blog = await Blog.findOne({
+			_user: req.user.id,
+			_id: req.params.id
+		});
 
-    res.send(blog);
-  });
+		res.send(blog);
+	});
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+	app.get('/api/blogs', requireLogin, async (req, res) => {
+		const client = redis.createClient(redisUrl);
+		const util = require('util');
+		client.get = util.promisify(client.get);
+		const id = req.user.id;
 
-    res.send(blogs);
-  });
+		const cachedBlogs = await client.get(id);
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+		if (cachedBlogs) {
+			console.log('SERVING FROM CACHE');
+			return res.send(JSON.parse(cachedBlogs));
+		}
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
-    });
+		const blogs = await Blog.find({_user: id});
+		client.set(id, JSON.stringify(blogs))
+		console.log('SERVING FROM MONGO_DB');
+		res.send(cachedBlogs);
+	});
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+	app.post('/api/blogs', requireLogin, async (req, res) => {
+		const {title, content} = req.body;
+
+		const blog = new Blog({
+			title,
+			content,
+			_user: req.user.id
+		});
+
+		try {
+			await blog.save();
+			res.send(blog);
+		} catch (err) {
+			res.send(400, err);
+		}
+	});
 };
