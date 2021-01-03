db56c14d936675467a64810a46163ca0174e61a9
Evgeniy Kvach <ypkvach@mts.ru>
2021-01-03 22:39:57 +0300

    add fetch tests

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -6,13 +6,65 @@ beforeEach(async () => {
 	page = await Page.build();
 	await page.gotoBase();
 })
-
 afterEach(async () => await page.close());
 
+describe('When logged in', async () => {
+	beforeEach(async () => {
+		await page.login();
+		await page.click('a.btn-floating');
+	})
+	test('can see blog create form', async () => {
+		const label = await page.getContentsOf('form label');
+		expect(label).toEqual('Blog Title')
+	})
+	describe('And using valid inputs', async () => {
+		beforeEach(async () => {
+			await page.type('.title input', 'MT')
+			await page.type('.content input', 'MC')
+			await page.click('form button')
+		})
+		test('Submitting takes user to review screen', async () => {
+			const text = await page.getContentsOf('h5');
+			expect(text).toEqual('Please confirm your entries');
+		})
+		test('Submitting then saving adds blog to index page', async () => {
+			await page.click('button.green');
+			await page.waitFor('.card');
 
-test('When logged in can see blog create form', async () => {
-	await page.login();
-	await page.click('a[href="/blogs/new"]');
-	const label = await page.getContentsOf('form label');
-	expect(label).toEqual('Blog Title')
+			const title = await page.getContentsOf('.card-title');
+			const content = await page.getContentsOf('p');
+			expect(title).toEqual('MT')
+			expect(content).toEqual('MC')
+		})
+	})
+})
+describe('User is not logged in', async () => {
+	const actions = [
+		{
+			method: 'get',
+			path: '/api/blogs'
+		},
+		{
+			method: 'post',
+			path: '/api/blogs',
+			data: {
+				title: 'T',
+				content: 'C'
+			}
+		}
+	];
+	test('Blog related actions are prohibited', async () => {
+		const results = await page.execRequests(actions);
+		results.forEach(r => {
+			expect(r).toEqual({error: 'You must log in!'})
+		})
+	})
+	test('get', async () => {
+		const result = await page.get('/api/blogs');
+		expect(result).toEqual({error: 'You must log in!'})
+	})
+	test('post', async () => {
+		const result = await page.post('/api/blogs', {title: 'T', content: 'C'});
+		expect(result).toEqual({error: 'You must log in!'})
+	})
 })
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -4,16 +4,18 @@ const userFactory = require('../factories/userFactory');
 
 class CustomPage {
 	static async build() {
-		const browser = await puppeteer.launch(({ headless: false}))
+		const browser = await puppeteer.launch(({headless: false}))
 		const page = await browser.newPage()
 		const customPage = new CustomPage(page)
 		return new Proxy(customPage, {
 			get: (target, prop) => customPage[prop] || browser[prop] || page[prop]
 		})
 	}
+
 	constructor(page) {
 		this.page = page;
 	}
+
 	async login() {
 		const user = await userFactory()
 		const {session, sig} = sessionFactory(user)
@@ -23,19 +25,54 @@ class CustomPage {
 		await this.gotoBlogs()
 		await this.waitFor('a[href="/auth/logout"]')
 	}
+
 	async getContentsOf(selector) {
 		return this.$eval(selector, el => el.innerHTML)
 	}
+
 	async gotoBase() {
 		return this.goto('localhost:3000')
 	}
+
 	async gotoHome() {
 		return this.goto('localhost:3000/home')
 
 	}
+
 	async gotoBlogs() {
 		return this.goto('localhost:3000/blogs')
 
+	}
+
+	get(path) {
+		return this.page.evaluate(url =>
+				fetch(url, {
+					method: 'GET',
+					credentials: 'same-origin',
+					headers: {
+						'Content-type': 'application/json'
+					}
+				})
+					.then(res => res.json()),
+			path)
+	}
+
+	post(path, data) {
+		return this.page.evaluate((url, _data) =>
+				fetch(url, {
+					method: 'POST',
+					credentials: 'same-origin',
+					headers: {
+						'Content-type': 'application/json'
+					},
+					body: JSON.stringify(_data)
+				})
+					.then(res => res.json()),
+			path, data)
+	}
+
+	execRequests(actions) {
+		return Promise.all(actions.map(({method, path, data}) => this[method](path, data)));
 	}
 }
 
